bbfe3f95104eb50e23e610a062f6401a76349b69
ahinthaj <Ahintha@infoxglobal.com>
2026-03-20 15:38:52 +0530

    Updated the import reportpage and lowstock and the route path

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import { AlertsPage }     from "./pages/AlertsPage";
 import { InventoryPage } from "./pages/InventoryPage";
 import { UserManagementPage } from "./pages/UserManagementPage";
 import { ZonesPage }       from "./pages/ZonesPage";
+import ReportsPage from "./pages/ReportsPage.tsx";
+import LowStockPage from "./pages/LowStockPage";
 
 // TODO: Add othrer pages and their routes here
 
@@ -36,6 +38,8 @@ export default function App() {
               <Route path="/alerts"             element={<AlertsPage />}     />
               <Route path="/inventory"          element={<InventoryPage />}  />
               <Route path="/zones"        element={<ZonesPage />} />
+              <Route path="/low-stock" element={<LowStockPage />} />
+              <Route path="/reports" element={<ReportsPage />} />
 
               {/* Admin-only routes */}
               {/* AdminRoute checks role. Non-admins are redirected to /dashboard. */}
